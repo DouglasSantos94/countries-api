6dcc52d92403d84c3dd2beeda81f5daf6876c6d6
Douglas <dgstos94@gmail.com>
2021-09-05 20:05:37 -0300

    remove typescript to test deploy

--- src/api/countryApi.ts
+++ src/api/countryApi.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import axios, { AxiosResponse } from "axios";
 
 const getCountries = (): Promise<AxiosResponse> =>
--- src/containers/CountryList/index.tsx
+++ src/containers/CountryList/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect, useState } from "react";
 import CountryItem from "components/CountryItem";
 import { getCountries, getCountriesByRegion, getCountry } from "api/countryApi";
