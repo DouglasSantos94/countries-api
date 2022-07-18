d7d337205be3509cce7bd9fd8ae95ac92a0929a9
Douglas Santos <douglas.santos@binamik.com.br>
2022-07-18 16:50:03 -0300

    refactoring: change API url due to discontinued

--- src/api/countryApi.ts
+++ src/api/countryApi.ts
@@ -3,20 +3,20 @@ import axios, { AxiosResponse } from "axios";
 
 const getCountries = (): Promise<AxiosResponse> =>
   axios.get(
-    "https://restcountries.eu/rest/v2/all?fields=name;nativeName;population;region;subregion;capital;topLevelDomain;currencies;languages;flag;borders;alpha3code"
+    "https://restcountries.com/v2/all?fields=name,population,region,subregion,capital,topLevelDomain,currencies,languages,flag,borders,alpha3code"
   );
 
 const getCountry = (countryName: string): Promise<AxiosResponse> =>
-  axios.get(`https://restcountries.eu/rest/v2/name/${countryName}`);
+  axios.get(`https://restcountries.com/v2/name/${countryName}`);
 
 const getCountriesByRegion = (regionFilter: string): Promise<AxiosResponse> =>
-  axios.get(`https://restcountries.eu/rest/v2/region/${regionFilter}`);
+  axios.get(`https://restcountries.com/v2/region/${regionFilter}`);
 
 const getCountryBorders = (borders: string[]): Promise<string[]> =>
   Promise.all(
     borders.map((b) =>
       axios
-        .get(`https://restcountries.eu/rest/v2/alpha/${b}`)
+        .get(`https://restcountries.com/v2/alpha/${b}`)
         .then(({ data: { name } }) => name)
         .catch((e) => console.log(e))
     )
